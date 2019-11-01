000a0090fc959251183f6515c92deb5aa93f17c9
Dante Mateo Bado Sucamele <dantebado@gmail.com>
2019-11-01 01:24:38 -0300

    Document URL Format

--- documents/src/main/java/nlp/frba/utn/documents/domain/Document.java
+++ documents/src/main/java/nlp/frba/utn/documents/domain/Document.java
@@ -2,12 +2,18 @@ package nlp.frba.utn.documents.domain;
 
 import java.util.Collection;
 
+import org.springframework.core.env.Environment;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import nlp.frba.utn.documents.domain.ner.NERAnalysis;
+import nlp.frba.utn.documents.utils.BeanUtil;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,8 +29,17 @@ public class Document {
 	private String quarter;
 	private String email;
 	private String documentName;
+	@JsonIgnore
 	private String documentURI;
 	
 	private NERAnalysis ner = new NERAnalysis();
+	
+	@Transient
+	@JsonProperty("document_uri")
+	public String formattedDocumentUri() {
+		Environment env = BeanUtil.getBean(Environment.class);
+		return documentURI.replace("%PATH_TO_FOLDER%", "http://" + env.getProperty("aws.publicip") + ":" + env.getProperty("server.port") + "/" +
+															env.getProperty("store.local.external-access") + env.getProperty("store.local.documents-relative-path"));
+	}
 
 }
